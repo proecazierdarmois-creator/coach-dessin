c94574bcd0efcb2a3ba11b2b1aaf4e3b852fd790
Eliel <pro.ecazierdarmois@gmail.com>
2026-06-02 17:14:22 +0000

    refactor: update login button and improve user display

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title="Test login")
+st.write("Query params:")
+st.write(st.query_params)
 
-st.write("USER:")
+st.write("User:")
 st.write(st.user)
 
-st.write("AUTH SECRETS:")
-st.write(st.secrets.get("auth", {}))
-
 if not st.user.is_logged_in:
-    st.button("Google", on_click=lambda: st.login("google"))
+    st.button(
+        "🔵 Continuer avec Google",
+        on_click=lambda: st.login("google")
+    )
     st.stop()
 
 st.success("Connecté")
-st.write(st.user.email)
-
-if st.button("Logout"):
-    st.logout()
-    st.rerun()
