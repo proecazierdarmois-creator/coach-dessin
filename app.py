22cd50ac748094c2938cd765d1da28a34a330605
Eliel <pro.ecazierdarmois@gmail.com>
2026-06-02 17:01:03 +0000

    debug google auth

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import streamlit as st
 
-st.write("is_logged_in =", st.user.is_logged_in)
-st.write("st.user =", st.user)
+st.set_page_config(page_title="Test login")
+
+st.write("USER:")
+st.write(st.user)
+
+st.write("AUTH SECRETS:")
+st.write(st.secrets.get("auth", {}))
 
 if not st.user.is_logged_in:
-    st.button(
-        "🔵 Continuer avec Google",
-        on_click=lambda: st.login("google")
-    )
+    st.button("Google", on_click=lambda: st.login("google"))
     st.stop()
 
-st.success("Connecté !")
+st.success("Connecté")
 st.write(st.user.email)
+
+if st.button("Logout"):
+    st.logout()
+    st.rerun()
